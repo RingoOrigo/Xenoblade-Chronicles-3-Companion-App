0f4836481abbe06a6a03c99f2fee293bda4a8bcc
Ringo Origo <nathaniel.t.wasko@gmail.com>
2023-04-21 13:02:20 -0400

    AboutFragment and Firebase

--- app/src/main/java/com/example/xenobladechronicles3companion/MonsterFragment.kt
+++ app/src/main/java/com/example/xenobladechronicles3companion/MonsterFragment.kt
@@ -6,11 +6,16 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.example.xenobladechronicles3companion.databinding.FragmentMonsterBinding
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.ktx.database
+import com.google.firebase.ktx.Firebase
 
 class MonsterFragment : Fragment() {
 
     private var _binding : FragmentMonsterBinding? = null
     private val binding get() = _binding!!
+
+    lateinit var dbref : DatabaseReference
 
     val monsters = listOf(
         Monster("Aeing Moramora", 26, "Pentelas Region", "Urayan Tunnels", false, false),
@@ -23,6 +28,8 @@ class MonsterFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         _binding = FragmentMonsterBinding.inflate(inflater, container, false)
+
+        dbref = Firebase.database.reference
 
         binding.monsterRecyclerView.adapter = MonsterAdapter(monsters)
 
