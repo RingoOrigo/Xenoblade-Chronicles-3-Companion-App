053bb66c51021a141476ec415304889e30eb7f2a
Ringo Origo <nathaniel.t.wasko@gmail.com>
2023-04-26 13:01:44 -0400

    Fully functional monster api call

--- app/src/main/java/com/example/xenobladechronicles3companion/MonsterViewHolder.kt
+++ app/src/main/java/com/example/xenobladechronicles3companion/MonsterViewHolder.kt
@@ -1,10 +1,22 @@
 package com.example.xenobladechronicles3companion
 
+import android.content.Intent
+import android.net.Uri
+import androidx.core.net.toUri
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
 import com.example.xenobladechronicles3companion.databinding.ListMonsterLayoutBinding
 
 class MonsterViewHolder (private val binding : ListMonsterLayoutBinding) : RecyclerView.ViewHolder(binding.root) {
     private lateinit var currentMonster : Monster
+
+    init {
+        binding.root.setOnClickListener {
+            val monsterURI = Uri.parse(currentMonster.articleurl)
+            val intent = Intent(Intent.ACTION_VIEW, monsterURI)
+            itemView.context.startActivity(intent)
+        }
+    }
 
     fun bindMonster(monster : Monster) {
         currentMonster = monster
@@ -13,5 +25,8 @@ class MonsterViewHolder (private val binding : ListMonsterLayoutBinding) : Recyc
         binding.levelNumericTextView.text = monster.level.toString()
         binding.areaTextView.text = monster.region
         binding.locationTextView.text = monster.location
+
+        val imageURI = currentMonster.imageurl.toUri().buildUpon().scheme("https").build()
+        Glide.with(itemView.context).load(imageURI).into(binding.monsterImageView)
     }
 }
